2cb26b908afeef2f293b2b517e544c9d5a01e6dc
pijenn <rafiezein2110@gmail.com>
2024-05-10 13:53:25 +0700

    ngebuat tentang segment 1 dan tujuan

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Segment4 from "./Compponent/HomeComp/segment4";
+import Segment1 from "./Compponent/AboutComp/Segment1";
+import SegmentTujuan from "./Compponent/AboutComp/SegmentTujuan";
 
 const About = () => {
-  return <div>Ini Tentang</div>;
+  return (
+    <>
+    <Segment1 />
+    <SegmentTujuan />
+    <Segment4 />
+    </>
+  )
+
 };
 
 export default About;
--- src/page/Compponent/AboutComp/Segment1.jsx
+++ src/page/Compponent/AboutComp/Segment1.jsx
@@ -1,44 +1,31 @@
 import React from "react";
 import background from "../../../assets/welcoming.svg";
-import gedung from "../../../assets/gedungFilkom.png";
-import arthakaraBubble from "../../../assets/arthakaraBubble.png";
-import awanKrem from "../../../assets/awanKrem.png";
+import awanKrem from "../../../assets/awanKrem.png"
 import { motion } from "framer-motion";
-import Atropos from "atropos/react";
 
-const Welcome = () => {
+const Segment1 = () => {
   return (
     <div className="overflow-hidden w-full h-full bg-[-100px] sm:bg-[-100px] md:bg-[0px] lg:bg-[0px] bg-cover" style={{ backgroundImage: `url(${background})` }}>
-      <Atropos
-        shadow={true}
-        shadowOffset={50}
-        highlight={true}
-        duration={100}
-        className="absolute group top-[220px] lg:right-[50px]  hidden lg:block"
-      >
-        <img
-          data-atropos-offset="15"
-          className="w-[150px] object-cover active:w-[100px]"
-          src={arthakaraBubble}
-          alt="MissingIMG"
-        />
-      </Atropos>
-      <div className="lg:flex lg:justify-center font-lato h-full pt-[170px] lg:pt-[150px]">
-        <div className="lg:mt-[60px] lg:mx-auto text-center pb-[70px] sm:pb-[50px] ml-[10px] sm:ml-[20px]">
-          <motion.div
-            initial={{ opacity: 0 }}
+      <div className="lg:flex-start lg:justify-start font-lato h-full pt-[100px] lg:pt-[80px] lg:pl-[50px]">
+        <div className="lg:mt-[60px] lg:mx-auto text-start pb-[70px] sm:pb-[50px] ml-[10px] sm:ml-[20px]">
+        <motion.div
+            initial={{ opacity: 0, x: -100 }}
             whileInView={{
               opacity: 1,
+              x: 0,
               transition: {
                 type: "spring",
-                duration: 5.0,
+                duration: 3.0,
                 damping: 15,
                 stiffness: 50,
               },
             }}
-            className="text-cust-white text-[20px] sm:text-[35px] md:w-full lg:text-start lg:text-[30px] font-regular"
+            className="text-[40px] sm:text-[60px] w-full h-[45px] sm:h-[60px] lg:h-[90px] lg:text-[65px] xl:text-[80px] text-cust-lime font-extrabold"
+            style={{
+              textShadow: "0 0 1.5px #BCDB37",
+            }}
           >
-            Selamat datang di
+          Tentang
           </motion.div>
           <motion.div
             initial={{ opacity: 0, x: -100 }}
@@ -78,23 +65,21 @@ const Welcome = () => {
           >
             UB 2024
           </motion.div>
-        </div>
-        <div className="mx-auto mt-auto lg:mt-0">
-          <motion.img
-            initial={{ opacity: 0, y: 50, x: 0 }}
+           <motion.div
+            initial={{ opacity: 0 }}
             whileInView={{
               opacity: 1,
-              y: 0,
               transition: {
-                type: "ease",
-                duration: 1.0,
-                damping: 10,
-                stiffness: 40,
+                type: "spring",
+                duration: 5.0,
+                damping: 15,
+                stiffness: 50,
               },
             }}
-            src={gedung}
-            className="lg:w-[550px] mx-auto mt-auto sm:w-[400px] w-[350px]"
-          />
+            className="text-cust-white text-[20px] sm:text-[35px] md:w-full lg:text-start lg:text-[30px] font-extrabold"
+          >
+            Kabinet Arthakara
+          </motion.div>
         </div>
       </div>
       <div>
@@ -104,4 +89,4 @@ const Welcome = () => {
   );
 };
 
-export default Welcome;
+export default Segment1;
